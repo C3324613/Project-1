60ae885fc579685286e4a7f17c1783fda4f61b7c
Elliott Shore <elliott.shore@gmail.com>
2019-04-02 02:18:08 +0000

    worked out how to encrypt and decrypt and implemented this

--- C3324613_commit1.c
+++ C3324613_commit1.c
@@ -2,14 +2,25 @@
 
 int main()
 {
-  
+  int index; // counter for encrypting and decrypting in for loops
   const int KEY = 2; // cipher key is equal to 15, written as KEY to show const
-  char message[] = "encrypt"; // this is the string that will be encrypted and later decrypted
+  char message[] = "encrypt "; // this is the string that will be encrypted and later decrypted
  
     printf("Cipher key is equal to %d\n", KEY); // print cipher key is equal to 15
   
     printf("Message to encrypt is: %s\n", message); // print message before encryption to show user
     
+    for(index = 0; message[index] != 0; index++){
+        message[index] = message[index] - KEY;
+    
+    }
+    printf("encrypted message is %s\n", message);
+    
+    for(index = 0; message[index] != 0; index++){
+        message[index] = message[index] + KEY;
+    }
+   
+    printf("decrypted message is %s\n", message);
 
 return 0;
 }
