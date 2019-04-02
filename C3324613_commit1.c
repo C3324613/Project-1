9585111190932f37dd8e612cc7c54fca520b54a7
Elliott Shore <elliott.shore@gmail.com>
2019-04-02 01:00:55 +0000

    init commit

--- C3324613_commit1.c
+++ C3324613_commit1.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+  
+  const int KEY = 15; // cipher key is equal to 15, written as KEY to show const
+  char message[25];
+  
+  printf("Cipher key is equal to %d\n", KEY); // print cipher key is equal to 15
 
-    
+  scanf("%s", message); // take user input as message to be encrypted
+  
+  printf("Message to encrypt is: %s\n", message); // print message before encryption
+
+
   return 0;
 }
