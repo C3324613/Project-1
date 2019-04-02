a5b76edbfe3883d7efde37b104b2714707a37cd6
Elliott Shore <elliott.shore@gmail.com>
2019-04-02 01:50:15 +0000

    changed key to 2 because I'm confused

--- C3324613_commit1.c
+++ C3324613_commit1.c
@@ -3,7 +3,7 @@
 int main()
 {
   
-  const int KEY = 9; // cipher key is equal to 15, written as KEY to show const
+  const int KEY = 2; // cipher key is equal to 15, written as KEY to show const
   char message[] = "encrypt"; // this is the string that will be encrypted and later decrypted
  
     printf("Cipher key is equal to %d\n", KEY); // print cipher key is equal to 15
