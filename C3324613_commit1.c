779014e928f0ca86a13985925a58f34310bbd088
Elliott Shore <elliott.shore@gmail.com>
2019-04-09 02:05:51 +0000

    changed my encryption formula to correct lower case letters into upper case and got rid of alphabet string Added an if statement which filters out characters which are not letters so they are not encrypted

--- C3324613_commit1.c
+++ C3324613_commit1.c
@@ -2,28 +2,34 @@
 
 int main()
 {
-  int index; // counter for encrypting and decrypting in for loops
+  int index; // counter for encrypting and decrypting in FOR loops
   const int KEY = 2; // cipher key is equal to 15, written as KEY to show const
-  char message[] = "encrypt"; // this is the string that will be encrypted and later decrypted
+  char message[] = "jacob is bad at programming%^&($#@!"; // this is the string that will be encrypted and later decrypted
  
     printf("Cipher key is equal to %d\n", KEY); // print cipher key is equal to 15
   
-    printf("Message to encrypt is: %s\n", message); // print message before encryption to show user
+    printf("Message to encrypt is : %s\n", message); // print message before encryption to show user
     
     for(index = 0; message[index] != 0; index++) { // takes each char in the string 'message' and assigns it a new ASCII value
         
-        message[index] = message[index] - KEY;    //e becomes c as 101 - 2 = 99
-    
-    }
-    
-    printf("encrypted message is %s\n", message);// prints to the new encrypted message to the console
-    
-    for(index = 0; message[index] != 0; index++) { // decrypts the encrypted message back to its original form
+        if((message[index] > 64 && message[index] < 91) || (message[index] > 96 && message[index] < 123)){  //checks for ASCII values which are not letters
+            
+            message[index] = ((message[index] - 65) + KEY) % 26 + 65;  
+            /* sets each letter in message to an ASCII value between 0-25 by minusing 65 as A = 65
+               adds the cipher key value to assign it a new ASCII value
+               takes the modulus of 26 to prevent characters being encrypted into characters which are not letters
+               adds 65 back to value to re-assign it an ASCII value which is a character                 
+            */
+           }
 
-        message[index] = message[index] + KEY;
+           
+        else{ }
+           
     }
+    
+    printf("encrypted message is : %s\n", message);// prints to the new encrypted message to the console
+    
    
-    printf("decrypted message is %s\n", message); // prints the decrypted message to the console
 
 return 0;
 }
