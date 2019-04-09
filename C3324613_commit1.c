cf39c0c5eb6bd6255f03c7705d64406efd1f165d
Elliott Shore <elliott.shore@gmail.com>
2019-04-09 02:47:53 +0000

    implemented previous code into a function for encryption added a decryption function that will execute however doesn't yet return the original value

--- C3324613_commit1.c
+++ C3324613_commit1.c
@@ -1,14 +1,34 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
-{
-  int index; // counter for encrypting and decrypting in FOR loops
-  const int KEY = 2; // cipher key is equal to 15, written as KEY to show const
-  char message[] = "jacob is bad at programming%^&($#@!"; // this is the string that will be encrypted and later decrypted
- 
-    printf("Cipher key is equal to %d\n", KEY); // print cipher key is equal to 15
-  
-    printf("Message to encrypt is : %s\n", message); // print message before encryption to show user
+char encryption(char* message, int index, const int KEY);
+char decryption(char* message, int index, const int KEY);
+
+
+int main(){
+    
+    int index = 0;
+    char message[] = "cat's and dog's";
+    const int KEY = 2;
+    
+    printf("Message to encrypt is : %s\n", message);
+    
+    encryption(message, index, KEY);
+    
+    printf("Encyrpted message is : %s\n", message);
+    
+    decryption(message, index, KEY);
+    
+    printf("Decrypted message is : %s\n", message);
+    
+    
+    
+    
+    
+    return 0;
+
+}
+
+char encryption(char* message, int index, const int KEY){
     
     for(index = 0; message[index] != 0; index++) { // takes each char in the string 'message' and assigns it a new ASCII value
         
@@ -21,15 +41,29 @@ int main()
                adds 65 back to value to re-assign it an ASCII value which is a character                 
             */
            }
-
            
         else{ }
            
     }
-    
-    printf("encrypted message is : %s\n", message);// prints to the new encrypted message to the console
-    
-   
+    return message[index];
+}
 
-return 0;
+char decryption(char* message, int index, const int KEY){
+    
+    for(index = 0; message[index] != 0; index++) { // takes each char in the string 'message' and assigns it a new ASCII value
+        
+        if((message[index] > 64 && message[index] < 91) || (message[index] > 96 && message[index] < 123)){  //checks for ASCII values which are not letters
+            
+            message[index] = ((message[index] - 65) - KEY) % 26 + 65;  
+            /* sets each letter in message to an ASCII value between 0-25 by minusing 65 as A = 65
+               adds the cipher key value to assign it a new ASCII value
+               takes the modulus of 26 to prevent characters being encrypted into characters which are not letters
+               adds 65 back to value to re-assign it an ASCII value which is a character                 
+            */
+           }
+           
+        else{ }
+           
+    }
+    return message[index];
 }
