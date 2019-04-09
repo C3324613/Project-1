fde941ef8cf6c04116cf0321cb05fc03fc2cbb68
Elliott Shore <elliott.shore@gmail.com>
2019-04-09 11:24:34 +0000

    Added a menu and switch statement function for menu
    it doesn't really do anything yet cause scanf sucks in che, will worry about making it work later

--- C3324613_commit1.c
+++ C3324613_commit1.c
@@ -1,13 +1,31 @@
 #include<stdio.h>
 
+
 char encryption(char* message, int index, const int KEY);
 char decryption(char* message, int index, const int KEY);
+int menu(int choice);
 
 
 int main(){
     
+    
+    printf("Welcome to my encryption and decryption program...\n\n");
+    printf("Please select from the following menu...\n\n");
+    printf("___________________________________\n");
+    printf("             M E N U               \n");
+    printf("------------------------------------\n\n");
+    printf("-1- Encrypt a message using a rotating cypher...\n");
+    printf("-2- Decrypt a message encrypted by a rotating cypher with a known key...\n");
+    printf("-3- Encrypt a message using a substituion cypher...\n\n");
+    printf("___________________________________\n\n");
+    printf("waiting for user input...\n\n\n");
+    
+    
+    
+    
+    
     int index = 0;
-    char message[] = "cat's and dog's";
+    char message[] = "JACOB";
     const int KEY = 2;
     
     printf("Message to encrypt is : %s\n", message);
@@ -19,30 +37,39 @@ int main(){
     decryption(message, index, KEY);
     
     printf("Decrypted message is : %s\n", message);
-    
-    
-    
-    
-    
+     
     return 0;
 
+}
+
+int menu(int choice){
+    
+    switch(choice){
+        case 1: printf("You have chosen to encrypt a message using a rotation cypher!\n");
+                break;
+        case 2: printf("You have chosen to decrypt a message encrypted by a rotational cypher with a known KEY!\n");
+                break;
+        case 3: printf("You have chosen to encrypt a message with a substition cypher\n");
+                break;
+        default: printf("INVALID INPUT...PLEASE TRY AGAIN\n");
+                break;
+    }
 }
 
 char encryption(char* message, int index, const int KEY){
     
     for(index = 0; message[index] != 0; index++) { // takes each char in the string 'message' and assigns it a new ASCII value
         
-        if((message[index] > 64 && message[index] < 91) || (message[index] > 96 && message[index] < 123)){  //checks for ASCII values which are not letters
+        if((message[index] > 65 || message[index] < 91 )){  //checks for ASCII values which are not letters
             
             message[index] = ((message[index] - 65) + KEY) % 26 + 65;  
-            /* sets each letter in message to an ASCII value between 0-25 by minusing 65 as A = 65
+            /* sets each letter in message to an ASCII value between 0-25 by minusing 65 as a = 65
                adds the cipher key value to assign it a new ASCII value
                takes the modulus of 26 to prevent characters being encrypted into characters which are not letters
                adds 65 back to value to re-assign it an ASCII value which is a character                 
             */
-           }
-           
-        else{ }
+       }
+
            
     }
     return message[index];
@@ -50,15 +77,12 @@ char encryption(char* message, int index, const int KEY){
 
 char decryption(char* message, int index, const int KEY){
     
-    for(index = 0; message[index] != 0; index++) { // takes each char in the string 'message' and assigns it a new ASCII value
+    for(index = 0; message[index] != 0; index++) { // cycles through each character in the string message
         
-        if((message[index] > 64 && message[index] < 91) || (message[index] > 96 && message[index] < 123)){  //checks for ASCII values which are not letters
+        if((message[index] > 65 || message[index] < 91)){  //checks for ASCII values which are not letters
             
-            message[index] = ((message[index] - 65) - KEY) % 26 + 65;  
-            /* sets each letter in message to an ASCII value between 0-25 by minusing 65 as A = 65
-               adds the cipher key value to assign it a new ASCII value
-               takes the modulus of 26 to prevent characters being encrypted into characters which are not letters
-               adds 65 back to value to re-assign it an ASCII value which is a character                 
+            message[index] = ((message[index] - 65) - KEY) + 65;  
+            /*                 
             */
            }
            
