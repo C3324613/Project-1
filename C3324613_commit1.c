254e6891e30736b2ee29ea7ff735c71b2d1e78d9
Elliott Shore <elliott.shore@gmail.com>
2019-04-16 01:48:38 +0000

    Added Substitution cipher and auto-correcting for lower case letters to upper case

--- C3324613_commit1.c
+++ C3324613_commit1.c
@@ -5,8 +5,9 @@
 
 char encryption(char* message, int index, const int KEY);
 char decryption(char* message, int index, const int KEY);
+char RotAttack(int index, char* message,char* freq, const int KEY);
 //int menu(int choice);
-
+char SubEncrypt(char* message, int index, char* SubKEY);
 
 int main(){
     
@@ -23,18 +24,38 @@ int main(){
     printf("waiting for user input...\n\n\n");*/
     
     int index = 0;
-    char message[] = "SJSFMPCRM WG O USBWIG. PIH WT MCI XIRUS O TWGV PM WHG OPWZWHM HC QZWAP O HFSS, WH KWZZ ZWJS WHG KVCZS ZWTS PSZWSJWBU HVOH WH WG GHIDWR. - OZPSFH SWBGHSWB";
-    const int KEY = 14;
+    char message[] = "PLEASE GET MILK !@#$%^&*()<>?";
+    //const int KEY = 14;
+    //char freq[26];
+    char SubKEY[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
 
-    /*printf("Message to encrypt is : %s\n", message);
+    for(index = 0; message[index] != 0; index++){
+       
+       if(message[index] >= 97 && message[index] <= 122){
+           
+           message[index] -= 32;
+       }
+       
+       
+    }
+   
+    printf("Message to encrypt is : %s\n", message);
     
-    encryption(message, index, KEY);*/
+    SubEncrypt(message, index, SubKEY);
+    
+    printf("Encrypted message via substitition is : %s\n", message);
+    
+    
+    
+    /*encryption(message, index, KEY);
    
     printf("Message to decrypt is : %s\n", message);
     
     decryption(message, index, KEY);
     
     printf("Decrypted message is : %s\n", message);
+    
+   // printf("Frequency string : %s\n", freq);*/
      
     return 0;
 
--- Head.h
+++ Head.h
@@ -18,7 +18,7 @@ char encryption(char* message, int index, const int KEY){
     
     for(index = 0; message[index] != 0; index++) { // takes each char in the string 'message' and assigns it a new ASCII value
         
-        if((message[index] > 64 || message[index] < 91 )){  //checks for ASCII values which are not letters. i.e punctuation characters, etc.   x x 
+        if((message[index] > 64 && message[index] < 91 )){  //checks for ASCII values which are not letters. i.e punctuation characters, etc.   x x 
             
             message[index] = (((message[index] - 65) + KEY) % 26) + 65;  
             /* sets each letter in message to an ASCII value between 0-25 by minusing 65 as a = 65
@@ -48,4 +48,136 @@ char decryption(char* message, int index, const int KEY){
     }
     return message[index];// returns the decrypted message in the string 'message'
 }
+
+char RotAttack(int index, char* message,char* freq, const int KEY){
+    
+    int count = 0;
+    int letter = 0;
+    int freqindex = 0;
+    
+    
+    for(index = 0; message[index] != 0; index++){
+        
+        letter = message[index];
+        
+        for(index = 0; message[index] != 0; index++){
+            
+            if(message[index] == letter){
+                
+                count++;
+            }
+            
+        
+        
+        }
+        
+        freq[freqindex] = count;
+        freqindex++;
+    
+    }
+    
+    return freq[freqindex];
+}
+
+char SubEncrypt(char* message, int index, char* SubKEY){
+    
+    int n;
+    
+        for(index = 0; message[index] != 0; index++){
+        
+            n = message[index];
+        
+            switch(n){
+            
+                case 65 : 
+                        message[index] = SubKEY[0];
+                        break;
+                case 66 :
+                        message[index] = SubKEY[1];
+                        break;
+                case 67 : 
+                        message[index] = SubKEY[2];
+                        break;
+                case 68 : 
+                        message[index] = SubKEY[3];
+                        break;
+                case 69 : 
+                        message[index] = SubKEY[4];
+                        break;
+                case 70 : 
+                        message[index] = SubKEY[5];
+                        break;
+                case 71 : 
+                        message[index] = SubKEY[6];
+                        break;
+                case 72 : 
+                        message[index] = SubKEY[7];
+                        break;
+                case 73 : 
+                        message[index] = SubKEY[8];
+                        break;
+                case 74 : 
+                        message[index] = SubKEY[9];
+                        break;
+                case 75 : 
+                        message[index] = SubKEY[10];
+                        break;
+                case 76 : 
+                        message[index] = SubKEY[11];
+                        break;
+                case 77 : 
+                        message[index] = SubKEY[12];
+                        break;
+                case 78 : 
+                        message[index] = SubKEY[13];
+                        break;
+                case 79 : 
+                        message[index] = SubKEY[14];
+                        break;
+                case 80 : 
+                        message[index] = SubKEY[15];
+                        break;
+                case 81 : 
+                        message[index] = SubKEY[16];
+                        break;
+                case 82 : 
+                        message[index] = SubKEY[17];
+                        break;
+                case 83 : 
+                        message[index] = SubKEY[18];
+                        break;
+                case 84 : 
+                        message[index] = SubKEY[19];
+                        break;
+                case 85 : 
+                        message[index] = SubKEY[20];
+                        break;
+                case 86 : 
+                        message[index] = SubKEY[21];
+                        break;
+                case 87 : 
+                        message[index] = SubKEY[22];
+                        break;
+                case 88 : 
+                        message[index] = SubKEY[23];
+                        break;
+                case 89 : 
+                        message[index] = SubKEY[24];
+                        break;
+                case 90 : 
+                        message[index] = SubKEY[25];
+                        break;
+                        
+                default :
+                        break;
+            }  
+       }
+
+    
+    return message[index];
+}
+
+
+
+
 #endif
