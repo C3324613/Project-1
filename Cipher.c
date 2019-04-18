b34b9114c97bd8e4c765c092b605e88afe81ae45
Elliott Shore <elliott.shore@gmail.com>
2019-04-18 05:58:50 +0000

    Added substitution decryption added a menu using switch cases

---
+++ Cipher.c
@@ -0,0 +1,195 @@
+#include<stdio.h>
+#include"Head.h"
+
+void RotEncrypt(char* message, int KEY);
+void RotDecrypt(char* message, int KEY);
+char RotAttack(int index, char* message, char* freq, int KEY);
+void SubEncrypt(char* message, char* SubKEY);
+void SubDecrypt(char* message, char* SDKEY);
+
+int main(){
+    
+    int choice = 0;
+    int index = 0; 
+    int i = 0;
+    int counter = 0;
+    int n = 65;
+    int KEY = 14;// key for rotation cipher
+    //char freq[26];
+    char k[] =      "11111111111111111111111111"; // key string for rotational cipher, only used to display info to user
+    char a[] =      "ABCDEFGHIJKLMNOPQRSTUVWXYZ";// used to generate the decrytpion string 'SDKEY' for substitution
+    char SubKEY[] = "11111111111111111111111111";// substitution cipher encryption key
+    char SDKEY[] =  "11111111111111111111111111";// placeholder for auto generated substitution decryption key
+    char message[1024];// string that will store message that will be encrypted/ decrypted
+
+   printf("Please enter any message you would like to use throughout the program...\n\n");// prompts the user to enter a message they would like to decrypt
+   scanf("%[^\n]s", message); // takes the users input via stdin/out and stores it in the string 'message'
+   
+   for(index = 0; message[index] != 0; index++){
+       
+       if(message[index] >= 97 && message[index] <= 122){
+           
+           message[index] -= 32;
+       }
+       
+    } /*the above code checks for lowercase letters and changes them to uppercase
+        It does this by checking for ASCII values that match lowercase characters
+        and then minusing 32 from to convert to uppercase */
+   
+    printf("You entered : %s\n\n", message);// prints the users message to the terminal (will appear capitalised always)
+    printf("Choose a ciphering method by entering a number from the options below...\n\n");
+   
+    printf("||CIPHERING METHODS||\n\n");
+    printf("[1] Rotational cipher encryption\n\n");
+    printf("[2] Rotational cipher decryption with key\n\n");
+    printf("[3] Substitution cipher encryption\n\n");
+    printf("[4] Substitution cipher decryption with key\n\n");
+   
+    scanf("%d", &choice);
+   
+    switch(choice){
+       
+        case 1: printf("You chose Rotational cipher Encryption\n\n");
+                printf("Please enter a KEY to encrypt with between 0 and 26...\n\n");
+                scanf("%d", &KEY);
+                
+                for (index = 0; a[index] !=0; index++){
+    
+                    k[index] = ((a[index] - 65 + KEY) % 26) + 65;
+                }
+
+                printf("--%s--\n", a);
+                printf("--||||||||||||||||||||||||||--\n");
+                printf("--%s--\n\n", k);
+                printf("The KEY has been set to %d\n\n", KEY);
+                printf("The message you entered earlier will now be encrypted...\n\n");
+                printf("Message to encrypt is      : %s\n", message);
+                RotEncrypt(message, KEY);
+                printf("Encrypted message is       : %s\n", message);
+            
+                break;
+        case 2: printf("You chose Rotational cipher decryption with a known key\n\n");
+                printf("Please enter a KEY to decrypt with between 0 and 26...\n\n");
+                scanf("%d", &KEY);
+                
+                for (index = 0; a[index] !=0; index++){
+    
+                    k[index] = ((a[index] - 65 - KEY + 26) % 26) + 65;
+                }
+
+                printf("--%s--\n", k);
+                printf("--||||||||||||||||||||||||||--\n");
+                printf("--%s--\n\n", a);
+                printf("The KEY has been set to %d\n\n", KEY);
+                printf("The message you entered earlier will now be decrypted...\n\n");
+                printf("Message to decrypt is      : %s\n", message);
+                RotDecrypt(message, KEY);
+                printf("decrypted message is       : %s\n", message);
+                
+                break;
+        case 3: printf("You chose Substitution cipher encryption\n\n");
+                printf("Please assign each letter of the alphabet a new letter starting from A-Z\n\n");
+                scanf("%s", SubKEY);
+                
+                while( counter <= 26){
+			
+	               for(index = 0; SubKEY[index] != 0; index++){
+						
+	                   if ((SubKEY[index]) == n){
+					
+					       SDKEY[i] = a[index];	
+		              }	
+			
+	               }
+			
+		          counter++;
+                  n++;
+		          i++;
+		          index = 0;
+                }
+                
+                printf("--%s--\n", a);
+                printf("--||||||||||||||||||||||||||--\n");
+                printf("--%s--\n\n", SubKEY);
+                printf("The KEY has been set to %s\n\n", SubKEY);
+                printf("The message you entered earlier will now be encrypted...\n\n");
+                printf("Message to encrypt is      : %s\n", message);
+                SubEncrypt(message, SubKEY);
+                printf("encrypted message is       : %s\n", message);
+                
+                break;
+        case 4: printf("You chose Substitution cipher decryption with a known key\n\n");
+                printf("Please enter the string of letters/ KEY used to encrypt the message you want to decrypt\n\n");
+                printf("The program will auto-generate a decryption key based on your encryption key\n\n");
+                scanf("%s", SubKEY);
+                printf("Generating...\n\n");
+                
+                while( counter <= 26){
+			
+	               for(index = 0; SubKEY[index] != 0; index++){
+						
+	                   if ((SubKEY[index]) == n){
+					
+					       SDKEY[i] = a[index];
+        				   printf("KEY : %s\n", SDKEY);	
+		              }	
+			
+	               }
+			
+		          counter++;
+                  n++;
+		          i++;
+		          index = 0;
+                }
+                
+                printf("\n");
+                printf("--%s--\n", SubKEY);
+                printf("--||||||||||||||||||||||||||--\n");
+                printf("--%s--\n\n", SDKEY);
+                printf("The KEY has been set to %s\n\n", SDKEY);
+                printf("The message you entered earlier will now be decrypted...\n\n");
+                printf("Message to decrypt is      : %s\n", message);
+                SubDecrypt(message, SDKEY);
+                printf("Decrypted message is       : %s\n", message);
+                
+                break;
+        default: printf("INVALID CHOICE");
+                break;
+           
+   }
+    
+    
+    
+    
+    
+
+
+    
+    
+    //RotEncrypt(message, KEY);
+    
+    //printf("Message is : %s\n", message);
+    
+    /*SubEncrypt(message, SubKEY);
+    
+    printf("Encrypted message via substitution is : %s\n", message);
+    
+    SubDecrypt(message, SDKEY);
+    
+    printf("Decrypted message is : %s\n", message);
+    
+    
+    
+    RotEncrypt(message, KEY);
+   
+    printf("Message to decrypt is : %s\n", message);
+    
+    RotDecrypt(message, KEY);
+    
+    printf("Decrypted message is : %s\n", message);
+    
+   // printf("Frequency string : %s\n", freq);*/
+     
+    return 0;
+
+}
--- Head.h
+++ Head.h
@@ -1,20 +1,9 @@
 #ifndef VARIABLE
 #define VARIABLE
-/*int menu(int choice){
-    
-    switch(choice){
-        case 1: printf("You have chosen to encrypt a message using a rotation cypher!\n");
-                break;
-        case 2: printf("You have chosen to decrypt a message encrypted by a rotational cypher with a known KEY!\n");
-                break;
-        case 3: printf("You have chosen to encrypt a message with a substition cypher\n");
-                break;
-        default: printf("INVALID INPUT...PLEASE TRY AGAIN\n");
-                break;
-    }
-}*/
 
-char encryption(char* message, int index, const int KEY){
+void RotEncrypt(char* message, const int KEY){
+    
+    int index = 0;
     
     for(index = 0; message[index] != 0; index++) { // takes each char in the string 'message' and assigns it a new ASCII value
         
@@ -29,10 +18,12 @@ char encryption(char* message, int index, const int KEY){
         }
       
     }
-    return message[index];// returns the new encrypted message in the string 'message'
+    
 }
 
-char decryption(char* message, int index, const int KEY){
+void RotDecrypt(char* message, const int KEY){
+    
+    int index = 0;
     
     for(index = 0; message[index] != 0; index++) { // cycles through each character in the string message
         
@@ -46,7 +37,7 @@ char decryption(char* message, int index, const int KEY){
         } 
            
     }
-    return message[index];// returns the decrypted message in the string 'message'
+    
 }
 
 char RotAttack(int index, char* message,char* freq, const int KEY){
@@ -79,9 +70,10 @@ char RotAttack(int index, char* message,char* freq, const int KEY){
     return freq[freqindex];
 }
 
-char SubEncrypt(char* message, int index, char* SubKEY){
+void SubEncrypt(char* message, char* SubKEY){
     
     int n;
+    int index = 0;
     
         for(index = 0; message[index] != 0; index++){
         
@@ -166,18 +158,112 @@ char SubEncrypt(char* message, int index, char* SubKEY){
                         break;
                 case 90 : 
                         message[index] = SubKEY[25];
-                        break;
-                        
+                        break;     
                 default :
                         break;
             }  
        }
 
     
-    return message[index];
+   
 }
 
+void SubDecrypt(char* message, char* SDKEY){
+    
+    int n;
+    int index = 0;
+    
+        for(index = 0; message[index] != 0; index++){
+        
+            n = message[index];
+        
+            switch(n){
+            
+                case 65 : 
+                        message[index] = SDKEY[0];
+                        break;
+                case 66 :
+                        message[index] = SDKEY[1];
+                        break;
+                case 67 : 
+                        message[index] = SDKEY[2];
+                        break;
+                case 68 : 
+                        message[index] = SDKEY[3];
+                        break;
+                case 69 : 
+                        message[index] = SDKEY[4];
+                        break;
+                case 70 : 
+                        message[index] = SDKEY[5];
+                        break;
+                case 71 : 
+                        message[index] = SDKEY[6];
+                        break;
+                case 72 : 
+                        message[index] = SDKEY[7];
+                        break;
+                case 73 : 
+                        message[index] = SDKEY[8];
+                        break;
+                case 74 : 
+                        message[index] = SDKEY[9];
+                        break;
+                case 75 : 
+                        message[index] = SDKEY[10];
+                        break;
+                case 76 : 
+                        message[index] = SDKEY[11];
+                        break;
+                case 77 : 
+                        message[index] = SDKEY[12];
+                        break;
+                case 78 : 
+                        message[index] = SDKEY[13];
+                        break;
+                case 79 : 
+                        message[index] = SDKEY[14];
+                        break;
+                case 80 : 
+                        message[index] = SDKEY[15];
+                        break;
+                case 81 : 
+                        message[index] = SDKEY[16];
+                        break;
+                case 82 : 
+                        message[index] = SDKEY[17];
+                        break;
+                case 83 : 
+                        message[index] = SDKEY[18];
+                        break;
+                case 84 : 
+                        message[index] = SDKEY[19];
+                        break;
+                case 85 : 
+                        message[index] = SDKEY[20];
+                        break;
+                case 86 : 
+                        message[index] = SDKEY[21];
+                        break;
+                case 87 : 
+                        message[index] = SDKEY[22];
+                        break;
+                case 88 : 
+                        message[index] = SDKEY[23];
+                        break;
+                case 89 : 
+                        message[index] = SDKEY[24];
+                        break;
+                case 90 : 
+                        message[index] = SDKEY[25];
+                        break;     
+                default :
+                        break;
+            }  
+       }
 
-
+    
+   
+}
 
 #endif
